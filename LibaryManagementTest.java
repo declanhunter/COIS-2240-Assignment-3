8dee7d020025aa9c633ad67c25de0b498e49e2b9
Declan Hunter <declanhunter156@icloud.com>
2024-12-02 14:09:02 -0500

    3rd test for borrowing and returning

--- LibaryManagementTest.java
+++ LibaryManagementTest.java
@@ -1,8 +1,11 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class LibaryManagementTest {
+	
+	@Test 
 	public void testBookId() {
 		try {
 			Book low = new Book(100, "book is low");
@@ -31,5 +34,38 @@ class LibaryManagementTest {
 		assertEquals("Invalid Book ID", toHigh.getMessage());
 		
 	}
+	
+	private Book book; 
+	private Member member; 
+	private Transaction transaction;
+	
+	@BeforeEach
+	public void setUp() throws Exception {
+		
+		book = new Book(101, "Test Book");
+		member = new Member(1, "Test Member");
+		transaction = Transaction.getTransaction();
+	}
+	
+	@Test 
+	public void testBorrowReturn() {
+		assertTrue(book.isAvailable(), "Book should be available intially");
+		
+		boolean borrow = transaction.borrowBook(book, member);
+		assertTrue(borrow, "borrowing should work");
+		assertFalse(book.isAvailable(), "book should no longer be available");
+		
+		boolean borrowTwo = transaction.borrowBook(book, member);
+		assertFalse(borrowTwo, "should fail as book is borrowed");
+		
+		boolean returnBook = transaction.returnBook(book, member);
+		assertTrue(returnBook, "return should be successful");
+		assertTrue(book.isAvailable(), "book should be available");
+		
+		boolean returnTwo = transaction.returnBook(book, member);
+		assertFalse(returnTwo, "returning same book should not work");
+		
+	}
+	
 	
 }
